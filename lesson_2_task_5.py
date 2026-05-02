1ebeaeee0c0aae5dd8a8fb9bb6be5de8d8a0e3dc
Sergey <gromoverzhetss@mail.ru>
2026-05-02 13:36:48 +0300

    Update lesson_2_task_5.py

--- lesson_2_task_5.py
+++ lesson_2_task_5.py
@@ -1,1 +1,12 @@
-
+def month_to_season(month):
+    if 12 >= month <= 2:
+        return "Зима"
+    if 3 <= month <= 5:
+        return "Весна"
+    if 6 <= month <= 8:
+        return "Лето"
+    if 9 <= month <= 11:
+        return "Осень"
+    return "Такого нет"
+month = int(input("Введите номер месяца: "))
+print(month_to_season(month))
