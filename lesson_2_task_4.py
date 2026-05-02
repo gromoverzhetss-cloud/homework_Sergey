af7b5fd1930da39239dfc0c6c5669ccad572ebb8
Sergey <gromoverzhetss@mail.ru>
2026-05-02 13:07:11 +0300

    Update lesson_2_task_4.py

--- lesson_2_task_4.py
+++ lesson_2_task_4.py
@@ -1,1 +1,12 @@
-
+n = int(input("Введите число: "))
+def fizz_buzz(n):
+     for i in range(1, n + 1):
+      if i % 3 == 0:
+       print(f"{i} Fizz")
+     if i % 5 == 0:
+      print(f"{i} Buzz")
+     if i % 3 == 0 and i % 5 == 0:
+         print(f"{i} fizzBuzz")
+     else:
+       print(i)
+fizz_buzz(n)
