058b8a3594172242e620b81e1578a4de4720c7ef
Sergey <gromoverzhetss@mail.ru>
2026-06-05 19:48:19 +0300

    Update test_lesson05_task1.py

--- 05_lesson/test_lesson05_task1.py
+++ 05_lesson/test_lesson05_task1.py
@@ -1,4 +1,4 @@
-from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -7,13 +7,11 @@ def test_navigation():
     driver = webdriver.Chrome()
     driver.get("https://httpbin.org/")
     driver.maximize_window()
-    sleep(20)
 
     driver.find_element(By.LINK_TEXT, "HTML form").click()
-    sleep(2)
     assert driver.current_url == "https://httpbin.org/forms/post"
     driver.back()
 
     assert driver.current_url == "https://httpbin.org/"
-    sleep(2)
+   
     driver.quit()
