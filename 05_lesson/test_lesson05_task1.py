dae5305e1f78ac144f26494cef3d5f37bd8759a5
Sergey <gromoverzhetss@mail.ru>
2026-06-05 13:55:13 +0300

    Update test_lesson05_task1.py

--- 05_lesson/test_lesson05_task1.py
+++ 05_lesson/test_lesson05_task1.py
@@ -1,1 +1,19 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+
+def test_navigation():
+    driver = webdriver.Chrome()
+    driver.get("https://httpbin.org/")
+    driver.maximize_window()
+    sleep(20)
+
+    driver.find_element(By.LINK_TEXT, "HTML form").click()
+    sleep(2)
+    assert driver.current_url == "https://httpbin.org/forms/post"
+    driver.back()
+
+    assert driver.current_url == "https://httpbin.org/"
+    sleep(2)
+    driver.quit()
