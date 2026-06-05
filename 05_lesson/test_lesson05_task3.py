6bd782b67deef96c6d321fac5a6b54cf6f5975b8
Sergey <gromoverzhetss@mail.ru>
2026-06-05 19:45:23 +0300

    Update test_lesson05_task3.py

--- 05_lesson/test_lesson05_task3.py
+++ 05_lesson/test_lesson05_task3.py
@@ -1,1 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+
+def test_multiple_elements():
+    driver = webdriver.Chrome()
+    driver.get("https://httpbin.org/links/10")
+    driver.maximize_window()
+
+    elements = driver.find_elements(By.TAG_NAME, 'a')
+    print(f"Найдено: {len(elements)}")
+    if elements == 10:
+        print("ok")
+    else:
+        print("no")
+
+    for element in elements:
+        assert element.is_displayed()
+    
+    if elements[0] == 1:
+        print(True)
+    else:
+        print(False)
+    driver.quit()
