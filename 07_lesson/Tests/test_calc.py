c380e3be5befa66143b1186b415835563b91bad5
Sergey <gromoverzhetss@mail.ru>
2026-07-20 11:49:06 +0300

    Update test_calc.py

--- 07_lesson/Tests/test_calc.py
+++ 07_lesson/Tests/test_calc.py
@@ -11,6 +11,12 @@ def driver():
     driver.maximize_window()
     yield driver
     driver.quit()
+    """
+    Фикстура для создания и управления экземпляром драйвера Chrome.
+
+    Создаёт экземпляр Chrome, разворачивает окно на весь экран,
+    предоставляет драйвер для теста, затем корректно закрывает браузер.
+    """
 
 @allure.feature("Тестирование калькулятора")
 @allure.severity("critical")
@@ -18,10 +24,18 @@ def driver():
 @allure.epic("КАЛЬКУЛЯТОР")
 @allure.description("Открытие сайта компании через переменную url")
 def test_calculator(driver):
+    """
+    Тест проверки работы калькулятора с задержкой.
+"""
     url = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
     page_calc = CalculatorPage(driver, url)
+    """Инициализировать страницу калькулятора"""
     page_calc.open()
+    """Открыть страницу"""
     page_calc.set_delay()
+    """Установить задержку (ввести 45)"""
     page_calc.enter_expression()
+    """Ввести выражение '7 + 8 ='"""
     page_calc.get_result()
+    """Получить и проверить результат вычисления"""
     assert page_calc.get_result() == "15"
